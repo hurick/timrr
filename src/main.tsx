9fb9c415189cccb43691f5a94ccb145aa1b9f5ac
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2022-09-30 16:39:10 -0300

    feat(styles): added global styles file

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,16 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyles } from './styles/global'
+import { lightTheme, darkTheme } from './styles/themes'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <h1>Timer</h1>
+    <ThemeProvider theme={darkTheme}>
+      <h1>Timer</h1>
+
+      <GlobalStyles />
+    </ThemeProvider>
   </StrictMode>
 )
